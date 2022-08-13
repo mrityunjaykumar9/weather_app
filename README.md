381d8da443f0844e086030c39e944c24a16effca
mrityunjaykumar9 <88943737+mrityunjaykumar9@users.noreply.github.com>
2022-08-13 12:03:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+    <b><a href="https://hweather.netlify.app" target="_blank">
+    <img src="./banner.png" />
+    </a></b>
+</div>
+
+## Installation
+
+```
+$ npm install
+```
+
+## **Thank You! ❣️**
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      <footer className="footer">
+        <a
+          href="https://github.com/mrityunjaykumar9"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i className="fa-brands fa-github"></i> <span>GitHub</span>
+        </a>
+        <a
+          href="https://github.com/mrityunjaykumar9"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i className ="fa-solid fa-code-fork"></i> <span>Fork</span>
+        </a>
+        <a
+          href="https://github.com/mrityunjaykumar9"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i className ="fa-solid fa-star"></i> <span>Star</span>
+        </a>
+        
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
